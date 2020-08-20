9190774b685a1cbd2093e1e375d5cab035d59956
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-20 19:58:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -14,3 +14,23 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
     puts "Hi, #{name}"
   end
 end
+
+
+def hello_t(array)
+  i = 0
+  #While i is less than the length of array
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+end
+# call your method here!
+#passes the array as an argument into  the method hello_t
+#method call block accepts |name| as a parameter
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    #checks to see if the name start_with? a 'T'
+  if name.start_with?("T")
+    #if so the do method puts out a greeting
+    puts "Hi, #{name}"
+  end
+end
